154efe578a23e79b4d1fd5710a0b3476276c4d5c
Jenil-Vekaria <jenilvekaria@outlook.com>
2023-11-22 15:48:48 -0500

    Create API route for note and dialog for note creation

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,11 +1,21 @@
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Quill - Notes",
 };
 
-const NotesPage = () => {
-  return <div>Here will be your notes</div>;
+const NotesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw Error("userId undefined");
+  }
+
+  const allNotes = await prisma.note.findMany({ where: { userId } });
+
+  return <div>{JSON.stringify(allNotes)}</div>;
 };
 
 export default NotesPage;
