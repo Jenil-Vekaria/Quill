e6242f902c6dd6c4bcf286b28664e5f7740e9198
Jenil-Vekaria <jenilvekaria@outlook.com>
2023-11-22 13:15:34 -0500

    setup clerk authentication

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Quill - Notes",
+};
+
+const NotesPage = () => {
+  return <div>Here will be your notes</div>;
+};
+
+export default NotesPage;
